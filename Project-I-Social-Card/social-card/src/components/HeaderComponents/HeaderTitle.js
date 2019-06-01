c5fb98d1d2038651bd1499ecc6dd84e37454165c
Danielle B <danibaxx@hotmail.com>
2019-06-01 12:11:00 -0600

    updated app.js with imports from HeaderContainer, ImageThumbnail, and HeaderTitle

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Header.css';
+import HeaderContent from './HeaderContent';
+
+const HeaderTitle = () => {
+  return (
+    <div className="header-title">
+      <p><strong>Lambda School</strong> @lambdaSchool • 26 jan </p>
+      <HeaderContent />
+    </div>
+  );
+};
+
+export default HeaderTitle;
