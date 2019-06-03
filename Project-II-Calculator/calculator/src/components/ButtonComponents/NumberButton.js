710301d44c48bf7eafac3c3afef1dc0d38ebe54f
Danielle B <danibaxx@hotmail.com>
2019-06-02 18:54:57 -0600

    added buttons to NumberButton.js

--- Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
+++ Project-II-Calculator/calculator/src/components/ButtonComponents/NumberButton.js
@@ -1,2 +1,45 @@
 import React from 'react';
 import './Button.css';
+
+const Buttons = () => {
+  return (
+    <div className="cal-wrap">
+      <input type="text" >
+        
+      </div>
+
+      <div className="row">
+        <button className={props.buttonStyle}>clear</button>
+        <button className={props.buttonStyle}>÷</button>
+      </div>
+
+      <div className="row">
+      <button className={props.buttonStyle}>7</button>
+      <button className={props.buttonStyle}>>8</button>
+      <button className={props.buttonStyle}>9</button>
+      <button className={props.buttonStyle}>×</button>
+      </div>
+
+      <div className="row">
+      <button className={props.buttonStyle}>4</button>
+      <button className={props.buttonStyle}>5</button>
+      <button className={props.buttonStyle}>6</button>
+      <button className={props.buttonStyle}>-</button>
+      </div>
+
+      <div className="row">
+      <button className={props.buttonStyle}>1</button>
+      <button className={props.buttonStyle}>2</button>
+      <button className={props.buttonStyle}>3</button>
+        <button className={props.buttonStyle}>+</button>
+      </div>
+
+      <div className="row">
+      <button className={props.buttonStyle}>0</button>
+      <button className={props.buttonStyle}>=</button>
+      </div>
+    </div>
+  );
+};
+
+export default Buttons;
